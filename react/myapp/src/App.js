b0f87878486b36e552279893019d3cf74202a47d
chahat <chahatgupta1060@gmail.com>
2023-12-30 13:28:39 +0530

    added react enviroment in project

---
+++ react/myapp/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+let a = 3;
+
+function App() {
+  return (
+     <div>
+      <h1>This is my first react app</h1>
+      <h2>Lets get started</h2>
+      
+     </div>
+  );
+}
+
+export default App;
